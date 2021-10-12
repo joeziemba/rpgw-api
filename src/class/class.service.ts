cd00b5643598a3d0721f4b4fb173b17c84cd807e
Joe Ziemba <jziemba90@gmail.com>
2021-10-12 00:24:07 -0700

    add findAll classes

--- src/class/class.service.ts
+++ src/class/class.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Ability } from 'src/common/constants';
 import { CreateClassDto } from './dto/create-class.dto';
 import { UpdateClassDto } from './dto/update-class.dto';
 import { Class, ClassDocument } from './schemas/class.schema';
@@ -21,8 +20,8 @@ export class ClassService {
     return newClass.save();
   }
 
-  findAll() {
-    return `This action returns all class`;
+  async findAll() {
+    return this.ClassModel.find();
   }
 
   findOne(id: number) {
